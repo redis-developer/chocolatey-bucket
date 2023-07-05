d090ce79f409b638220dc972938fffb16ea1eb17
jreleaserbot <jreleaser@kordamp.org>
2023-07-05 23:51:27 +0000

    riot v3.1.2

--- riot/tools/chocolateyinstall.ps1
+++ riot/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-03T07:40:52.34747229Z
+# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-05T23:49:08.84095428Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'riot-3.1.1'
+$app_home = Join-Path $package 'riot-3.1.2'
 $app_exe = Join-Path $app_home 'bin/riot.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'redis-riot' `
-    -Url 'https://github.com/redis-developer/riot/releases/download/v3.1.1/riot-3.1.1.zip' `
-    -Checksum 'a40960bfdd8a519eed66fd92cb3d5698df9147d33c8fe354a5ed514a9291360f' `
+    -Url 'https://github.com/redis-developer/riot/releases/download/v3.1.2/riot-3.1.2.zip' `
+    -Checksum '2557e6d16662d626a133c887b5bc8e9560ad9dac9b969df13fca982dd5553ee5' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- riot/tools/chocolateyuninstall.ps1
+++ riot/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-03T07:40:52.34747229Z
+# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-05T23:49:08.84095428Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'riot-3.1.1'
+$app_home = Join-Path $package 'riot-3.1.2'
 $app_exe = Join-Path $app_home 'bin/riot.bat'
 
 Uninstall-BinFile -Name 'riot' -Path $app_exe
